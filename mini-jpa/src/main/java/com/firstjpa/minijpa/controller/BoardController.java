f1848eb92f435b0975a9795d1f958e127ee9b60a
wonseok <nsk7640@naver.com>
2021-08-03 18:15:55 +0900

    글쓰기폼 , 게시판 완성

--- mini-jpa/src/main/java/com/firstjpa/minijpa/controller/BoardController.java
+++ mini-jpa/src/main/java/com/firstjpa/minijpa/controller/BoardController.java
@@ -1,25 +1,24 @@
 package com.firstjpa.minijpa.controller;
 
-import com.firstjpa.minijpa.MiniJpaApplication;
 import com.firstjpa.minijpa.controller.Form.BoardForm;
 import com.firstjpa.minijpa.domain.Board;
-import com.firstjpa.minijpa.domain.Photo;
 import com.firstjpa.minijpa.domain.User;
 import com.firstjpa.minijpa.service.BoardService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.security.Signature;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -60,27 +59,36 @@ public class BoardController {
     }
 
     //게시글 등록
+
+    @ResponseBody // ajax에 응답을 주기위해 사용한 어노테이션
     @PostMapping("/board/new2")
-    public String boardPost2(
+    public HashMap<String ,String> boardPost2(
             @Valid BoardForm boardForm ,
             BindingResult result ,
             HttpSession session ,
             @RequestParam(value = "files" , required = false) MultipartFile[] files,
-            @RequestParam(value = "test") String appendVal,
+//            @RequestParam(value = "test") String test,
             HttpServletRequest request) {
+
+        HashMap<String, String> returnMap = new HashMap<>();
 
 
         //Valid 체크
-        if (result.hasErrors()) {
-            return "board/boardForm";
-        }
-        System.out.println(appendVal);
-//        System.out.println(files[0].getOriginalFilename());
+        if (result.hasErrors()) { //valid통과 못한부분만 맵에 담아서 넘기기
+            returnMap.put("success","false");
+            for (Object object : result.getAllErrors()){
+                FieldError fieldError = (FieldError) object;
+                returnMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+            }
+            return returnMap;
 
-//        User user = (User)session.getAttribute("userinfo");
-//        Board board = Board.createBoard2(user, boardForm.getTitle(), boardForm.getContents() ,files);
-//        boardService.save(board);
-        return "redirect:/";
+        }
+        User user = (User)session.getAttribute("userinfo");
+        Board board = Board.createBoard2(user, boardForm.getTitle(), boardForm.getContents() ,files);
+        boardService.save(board);
+        returnMap.put("success","true");
+
+        return returnMap;
     }
 
 
