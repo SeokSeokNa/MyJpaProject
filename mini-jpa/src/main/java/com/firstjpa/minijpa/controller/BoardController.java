bcd237132d4efe42b2056f543f09e95fc31473d2
wonseok <nsk7640@naver.com>
2021-08-04 15:45:08 +0900

    도메인 수정

--- mini-jpa/src/main/java/com/firstjpa/minijpa/controller/BoardController.java
+++ mini-jpa/src/main/java/com/firstjpa/minijpa/controller/BoardController.java
@@ -5,8 +5,6 @@ import com.firstjpa.minijpa.domain.Board;
 import com.firstjpa.minijpa.domain.User;
 import com.firstjpa.minijpa.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.security.Signature;
 import java.util.HashMap;
 import java.util.List;
 
@@ -27,42 +24,19 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    //게시글 폼 이동
     @GetMapping("/board/new")
     public String boardForm(Model model) {
         model.addAttribute("boardForm" , new BoardForm());
         return "board/boardForm";
     }
 
-    @GetMapping("/board/new2")
-    public String boardForm2(Model model) {
-        model.addAttribute("boardForm" , new BoardForm());
-        return "board/boardForm2";
-    }
+
 
     //게시글 등록
-    @PostMapping("/board/new")
-    public String boardPost(
-            @Valid BoardForm boardForm ,
-            BindingResult result ,
-            HttpSession session ,
-            @RequestParam(value="uploadfile", required=false) List<MultipartFile> uploadfile,
-            HttpServletRequest request) {
-
-        //Valid 체크
-        if (result.hasErrors()) {
-            return "board/boardForm";
-        }
-        User user = (User)session.getAttribute("userinfo");
-//        Board board = Board.createBoard(user, boardForm.getTitle(), boardForm.getContents() ,uploadfile);
-//        boardService.save(board);
-        return "redirect:/";
-    }
-
-    //게시글 등록
-
     @ResponseBody // ajax에 응답을 주기위해 사용한 어노테이션
-    @PostMapping("/board/new2")
-    public HashMap<String ,String> boardPost2(
+    @PostMapping("/board/new")
+    public HashMap<String ,String> boardPost(
             @Valid BoardForm boardForm ,
             BindingResult result ,
             HttpSession session ,
@@ -91,18 +65,10 @@ public class BoardController {
         return returnMap;
     }
 
-
     @GetMapping("/board/list")
-    public String boardList(Model model) {
-        List<Board> boardList = boardService.boardList();
-        model.addAttribute("boardList",boardList);
-        return "/board/boardList";
-    }
-
-    @GetMapping("/board/list2")
     public String boardList2(Model model) {
         List<Board> boardList = boardService.boardList();
         model.addAttribute("boardList",boardList);
-        return "/board/boardList2";
+        return "board/boardList";
     }
 }
